e7f080fac9efae5aaa72e02767eb00f9d073e2ec
zhongti0126 <zhongti0126@users.noreply.github.com>
2026-05-11 01:33:30 +0800

    add delete feature

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let streak = 0;
 
-function addTask() {
+function createTaskElement(taskText) {
 
   const input = document.getElementById("taskInput");
   const taskList = document.getElementById("taskList");
@@ -10,7 +10,19 @@ function addTask() {
   }
 
   const li = document.createElement("li");
-  li.textContent = "✅ " + input.value;
+
+  li.innerHTML = `
+  ✅ ${taskText}
+  <button class="delete-btn">X</button>
+  ;
+
+  const deleteBtn = li.querySelector(".delete-btn");
+
+  deleteBtn.addEventListener("click", () => {
+
+  li.remove();
+
+  });
 
   taskList.appendChild(li);
 
