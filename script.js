1e7c7c4139d98ca2bad89f8aeac54a931719792e
zhongti0126 <zhongti0126@users.noreply.github.com>
2026-05-11 02:02:42 +0800

    fix script js

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
-let streak = 0;
+let streak = localStorage.getItem("streak") || 0;
 
-function createTaskElement(taskText) {
+document.getElementById("streak").textContent = streak;
 
+const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+savedTasks.forEach(task => {
+  createTaskElement(task);
+});
+
+function addTask() {
   const input = document.getElementById("taskInput");
-  const taskList = document.getElementById("taskList");
 
   if (input.value === "") {
     return;
   }
 
-  const li = document.createElement("li");
+  createTaskElement(input.value);
 
-  li.innerHTML = `
-  ✅ ${taskText}
-  <button class="delete-btn">X</button>
-  ;
-
-  const deleteBtn = li.querySelector(".delete-btn");
-
-  deleteBtn.addEventListener("click", () => {
-
-  li.remove();
-
-  });
-
-  taskList.appendChild(li);
+  savedTasks.push(input.value);
+  localStorage.setItem("tasks", JSON.stringify(savedTasks));
 
   input.value = "";
 
   streak++;
+  localStorage.setItem("streak", streak);
   document.getElementById("streak").textContent = streak;
+}
+
+function createTaskElement(taskText) {
+  const taskList = document.getElementById("taskList");
+
+  const li = document.createElement("li");
+
+  li.innerHTML = `
+    ✅ ${taskText}
+    <button class="delete-btn">X</button>
+  `;
+
+  const deleteBtn = li.querySelector(".delete-btn");
+
+  deleteBtn.addEventListener("click", () => {
+    li.remove();
+  });
+
+  taskList.appendChild(li);
 }
